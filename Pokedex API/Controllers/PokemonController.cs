002727c66a9d8e6d4adf5017f2f269558860a7f2
Jędrzej Kłyza <neshernos@gmail.com>
2022-11-09 23:52:32 +0100

    Add project files.

---
+++ Pokedex API/Controllers/PokemonController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pokedex_API.Models;
+
+namespace Pokedex_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Pokemon>>> Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Pokedex API/Models/Pokemon.cs
@@ -0,0 +1,10 @@
+﻿namespace Pokedex_API.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Height { get; set; }
+        public float Weight { get; set; }
+    }
+}
