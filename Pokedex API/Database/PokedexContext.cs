1b65b4f1c994694915f1708ccbbb76bdb3b44653
Jędrzej Kłyza <neshernos@gmail.com>
2022-11-10 11:59:30 +0100

    Added PokedexContext and connectionString to local database

---
+++ Pokedex API/Database/PokedexContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pokedex_API.Models;
+
+namespace Pokedex_API.Database
+{
+    public class PokedexContext : DbContext
+    {
+        public DbSet<Ability> Abilities => Set<Ability>();
+        public DbSet<AbilityCategory> AbilityCategories => Set<AbilityCategory>();
+        public DbSet<Pokemon> Pokemons => Set<Pokemon>();
+        public DbSet<PokemonCategory> PokemonCategories => Set<PokemonCategory>();
+        public DbSet<PokemonType> PokemonTypes => Set<PokemonType>();
+
+        public PokedexContext(DbContextOptions<PokedexContext> options) : base(options)
+        {
+        }
+    }
+}
