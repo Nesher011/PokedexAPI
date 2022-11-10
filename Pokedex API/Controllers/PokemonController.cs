9a2876cda09e35b785d26e63d50759ce47391a46
Jędrzej Kłyza <neshernos@gmail.com>
2022-11-10 01:59:43 +0100

    Created PokemonController and IPokemonRepository

--- Pokedex API/Controllers/PokemonController.cs
+++ Pokedex API/Controllers/PokemonController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Pokedex_API.Interfaces;
 using Pokedex_API.Models;
 
 namespace Pokedex_API.Controllers
@@ -7,10 +8,53 @@ namespace Pokedex_API.Controllers
     [Route("api/[controller]")]
     public class PokemonController : ControllerBase
     {
-        [HttpGet]
-        public async Task<ActionResult<List<Pokemon>>> Get()
+        private readonly IPokemonRepository _pokemonRepository;
+
+        public PokemonController(IPokemonRepository pokemonRepository)
         {
-            return Ok();
+            _pokemonRepository = pokemonRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Pokemon>>> GetAllPokemons()
+        {
+            return Ok(await _pokemonRepository.GetAllPokemons());
+        }
+
+        [HttpGet("{type}")]
+        public async Task<ActionResult<List<Pokemon>>> GetPokemonsByType(Type type)
+        {
+            return null;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Pokemon>>> GetPokemon(int id)
+        {
+            return await _pokemonRepository.GetPokemon(id) is Pokemon p ? Ok(p) : NotFound();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Pokemon>> Add(Pokemon pokemon)
+        {
+            await _pokemonRepository.Add(pokemon);
+
+            return Ok(pokemon);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Pokemon>> Update(int id, Pokemon pokemon)
+        {
+            await _pokemonRepository.Update(id, pokemon);
+
+            return Ok(pokemon);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _pokemonRepository.Delete(id);
+
+            return NoContent();
         }
     }
 }
---
+++ Pokedex API/Interfaces/IPokemonRepository.cs
@@ -0,0 +1,19 @@
+﻿using Pokedex_API.Models;
+
+namespace Pokedex_API.Interfaces
+{
+    public interface IPokemonRepository
+    {
+        Task<List<Pokemon>> GetAllPokemons();
+
+        Task<List<Pokemon>> GetPokemonsByType(PokemonType type);
+
+        Task<Pokemon> GetPokemon(int id);
+
+        Task<Pokemon> Add(Pokemon pokemon);
+
+        Task<Pokemon> Update(int id, Pokemon pokemon);
+
+        Task Delete(int id);
+    }
+}
