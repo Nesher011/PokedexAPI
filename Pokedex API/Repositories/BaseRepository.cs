ef5be844812c491b16e8cfa2b4d52d7d6401f458
Jędrzej Kłyza <neshernos@gmail.com>
2022-11-10 12:01:13 +0100

    Created BaseRepository and PokemonRepository

---
+++ Pokedex API/Repositories/BaseRepository.cs
@@ -0,0 +1,57 @@
+﻿using Pokedex_API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Pokedex_API.Database;
+using Pokedex_API.Models;
+
+namespace Pokedex_API.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
+    {
+        private readonly PokedexContext _dbContext;
+
+        public BaseRepository(PokedexContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<T> Create(T entity)
+        {
+            if (entity == null)
+                throw new NotImplementedException();
+
+            await _dbContext.SaveChangesAsync();
+
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Pokedex API/Repositories/PokemonRepository.cs
@@ -0,0 +1,21 @@
+﻿using Pokedex_API.Database;
+using Pokedex_API.Interfaces;
+using Pokedex_API.Models;
+
+namespace Pokedex_API.Repositories
+{
+    public class PokemonRepository : BaseRepository<Pokemon>, IPokemonRepository
+    {
+        private readonly PokedexContext _dbContext;
+
+        public PokemonRepository(PokedexContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<List<Pokemon>> GetByType(PokemonType type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
