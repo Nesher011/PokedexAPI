09a42f6f1f70794e575f694fe5275b373f46baae
Jędrzej Kłyza <neshernos@gmail.com>
2022-11-10 12:00:54 +0100

    Changed int id to guid id

--- Pokedex API/Controllers/PokemonController.cs
+++ Pokedex API/Controllers/PokemonController.cs
@@ -18,25 +18,25 @@ namespace Pokedex_API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Pokemon>>> GetAllPokemons()
         {
-            return Ok(await _pokemonRepository.GetAllPokemons());
+            return Ok(await _pokemonRepository.GetAll());
         }
 
         [HttpGet("{type}")]
         public async Task<ActionResult<List<Pokemon>>> GetPokemonsByType(PokemonType type)
         {
-            return Ok(await _pokemonRepository.GetPokemonsByType(type));
+            return Ok(await _pokemonRepository.GetByType(type));
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<List<Pokemon>>> GetPokemon(int id)
         {
-            return await _pokemonRepository.GetPokemon(id) is Pokemon p ? Ok(p) : NotFound();
+            return await _pokemonRepository.GetById(id) is Pokemon p ? Ok(p) : NotFound();
         }
 
         [HttpPost]
         public async Task<ActionResult<Pokemon>> Add(Pokemon pokemon)
         {
-            await _pokemonRepository.Add(pokemon);
+            await _pokemonRepository.Create(pokemon);
 
             return Ok(pokemon);
         }
@@ -44,15 +44,15 @@ namespace Pokedex_API.Controllers
         [HttpPut("{id}")]
         public async Task<ActionResult<Pokemon>> Update(int id, Pokemon pokemon)
         {
-            await _pokemonRepository.Update(id, pokemon);
+            await _pokemonRepository.Update(pokemon);
 
             return Ok(pokemon);
         }
 
         [HttpDelete("{id}")]
-        public async Task<ActionResult> Delete(int id)
+        public async Task<ActionResult> Delete(Guid guid)
         {
-            await _pokemonRepository.Delete(id);
+            await _pokemonRepository.Delete(guid);
 
             return NoContent();
         }
