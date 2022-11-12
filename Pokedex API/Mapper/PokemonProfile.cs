f44c900eef234507c5ff58bf8ad4eb45a568d7a9
Jędrzej Kłyza <neshernos@gmail.com>
2022-11-12 17:00:01 +0100

    Added PokemonProfile for Mapper

---
+++ Pokedex API/Mapper/PokemonProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Pokedex_API.Dtos;
+using Pokedex_API.Models;
+
+namespace Pokedex_API.Mapper
+{
+    public class PokemonProfile:Profile
+    {
+        public PokemonProfile()
+        {
+            CreateMap<PokemonDto, Pokemon>().ReverseMap();
+        }
+    }
+}
